db3650bb5e653964a307bcd9da6ee9068b981325
oleksharh <oleksharh@gmail.com>
2024-05-31 21:56:15 +0100

    updated fetching

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -6,29 +6,29 @@ import csv
 pools_list = []
 
 # Fetch POOL ADDRESSES
-for i in range(1, 11):
-    pools_url = "https://api.geckoterminal.com/api/v2/networks/ton/pools"
-    base_params = {
-        "network": "ton",
-        "page": i,
-    }
+# for i in range(1, 11):
+#     pools_url = "https://api.geckoterminal.com/api/v2/networks/ton/pools"
+#     base_params = {
+#         "network": "ton",
+#         "page": i,
+#     }
 
-    response = requests.get(pools_url, params=base_params)
-    results = response.json()
+#     response = requests.get(pools_url, params=base_params)
+#     results = response.json()
 
-    if "data" in results and len(results["data"]) > 0:
-        keys_to_print = ["name", "address"]
+#     if "data" in results and len(results["data"]) > 0:
+#         keys_to_print = ["name", "address"]
 
-        for pool in results["data"]:
-            lim_pool_inf = {key: pool["attributes"][key] for key in keys_to_print}
-            pools_list.append(lim_pool_inf["address"])
+#         for pool in results["data"]:
+#             lim_pool_inf = {key: pool["attributes"][key] for key in keys_to_print}
+#             pools_list.append(lim_pool_inf["address"])
 
-            print(lim_pool_inf)
-    else:
-        print("No pools found.")
-    print(f"Page {i}")
+#             print(lim_pool_inf)
+#     else:
+#         print("No pools found.")
+#     print(f"Page {i}")
 
-print("Pool addresses:", pools_list)
+# print("Pool addresses:", pools_list)
 
 
 # with open("pool_list.csv", mode="w", newline='') as file:
@@ -39,7 +39,10 @@ print("Pool addresses:", pools_list)
 
 
 
-
+with open("C:\Python\crypto-price-tracker\pool_list.csv", mode='r', newline='') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        pools_list.append(row[0])
 
 # time.sleep(60)
 
@@ -51,11 +54,13 @@ print("Pool addresses:", pools_list)
 #     "currency": "usd",
 # }
 
+# pools_list = load_pools("C:/Python/crypto-price-tracker/pool-list.csv")
+
 params = { 
     "aggregate": 1,
     "limit": 10,
 }
-
+print(pools_list)
 for pool_address in pools_list:
     ohlcv_url = f"https://api.geckoterminal.com/api/v2/networks/ton/pools/{pool_address}/ohlcv/day"
     
@@ -69,7 +74,15 @@ for pool_address in pools_list:
 
     # if response.status_code == 200:
     results = response.json()
-    print(results)
+
+    ohlcv_list = results['data']['attributes']['ohlcv_list']
+    base_name = results['meta']['base']['name']
+    base_symbol = results['meta']['base']['symbol']
+    quote_name = results['meta']['quote']['name']
+    quote_symbol = results['meta']['quote']['symbol']
+
+    print(base_symbol, "->", quote_symbol, ohlcv_list, "5 days\n\n\n")
+    # print(results)
     #     if "data" in results and len(results["data"]) > 0:
     #         for pool in results["data"]:
     #             # Define the keys you want to print
--- app/populate_menu.py
+++ app/populate_menu.py
@@ -2,15 +2,24 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QVBoxLayout, QWidget
 from PyQt5.uic import loadUi
 
+
+# def load_stylesheet(filename):
+#     with open(filename, "r") as f:
+#         return f.read()
+    
+# stylesheet = load_stylesheet("styles/styles.css")
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("ui/interface_main_window.ui", self)
         self.populate_menu()
+
+        # self.setStyleSheet(stylesheet)
     
     def populate_menu(self):
         menu_layout = self.menu_widget.layout()
-        
+
         for i in range(1, 51):
             button = QPushButton(f"Label {i}", self)
             menu_layout.addWidget(button)
